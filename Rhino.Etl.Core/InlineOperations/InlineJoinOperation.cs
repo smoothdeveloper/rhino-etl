0a54c1d50cc848604e5d55de3f3542aa4a76610b
unknown <gauthier@.(none)>
2010-12-22 05:19:56 -0800

    * define InlineJoinOperation, InlineOperations allows to define common operations with inline syntax by passing functors, so simple processes / operations can be defined inline without implementing one class per operation * Unit test for ensureMergerReturnsNewRow behaviour (rationale for this feature: returning either left or right row is a common source of bugs if join is not 1-1 but is 1-N or N-1)

---
+++ Rhino.Etl.Core/InlineOperations/InlineJoinOperation.cs
@@ -0,0 +1,115 @@
+﻿namespace Rhino.Etl.Core.InlineOperations {
+
+	using System;
+	using Rhino.Etl.Core.Operations;
+
+	/// <summary>
+	/// 'Inline' implementation of a <see cref="JoinOperation"/>
+	/// </summary>
+	public class InlineJoinOperation : JoinOperation {
+
+		private JoinType joinType;
+		private string[] leftColumnNames;
+		private string[] rightColumnNames;
+		private Func<Row, Row, Row> rowMerger;
+		private bool ensureMergerReturnsNewRow;
+
+		/// <summary>
+		/// initializes a bare <see cref="JoinOperation"/> with everything required
+		/// </summary>
+		/// <param name="leftColumnNames">column names of the left row to join on</param>
+		/// <param name="rightColumnNames">column names of the right row to join on</param>
+		/// <param name="joinType"><see cref="JoinType"/> to perform</param>
+		/// <param name="rowMerger">A functor reference which takes left and right <see cref="Row"/> and return a merged <see cref="Row"/></param>
+		/// <param name="leftOperation">left <see cref="IOperation"/></param>
+		/// <param name="rightOperation">right <see cref="IOperation"/></param>
+		/// <param name="ensureMergerReturnsNewRow">if true, will throw an <see cref="InvalidOperationException"/> if rowMerger returns any of left or right row</param>
+		public InlineJoinOperation(string[] leftColumnNames, string[] rightColumnNames, JoinType joinType, Func<Row, Row, Row> rowMerger, IOperation leftOperation, IOperation rightOperation, bool ensureMergerReturnsNewRow) {
+			this.leftColumnNames = leftColumnNames;
+			this.rightColumnNames = rightColumnNames;
+			this.joinType = joinType;
+			this.rowMerger = rowMerger;
+			this.ensureMergerReturnsNewRow = ensureMergerReturnsNewRow;
+			Left(leftOperation);
+			Right(rightOperation);
+		}
+
+		/// <summary>
+		/// Used on <see cref="LeftOrphanRow"/> if not null
+		/// </summary>
+		public Action<Row> LeftOrphanHandler { get; set; }
+		
+		/// <summary>
+		/// Used on <see cref="RightOrphanRow"/> if not null
+		/// </summary>
+		public Action<Row> RightOrphanHandler { get; set; }
+
+		/// <summary>
+		/// Allows to define name that is not <c>this.GetType().Name</c> name 
+		/// This name is returned by <see cref="Name"/> if not null or empty
+		/// </summary>
+		public string OverridedName { get; set; }
+
+		/// <inheriteddocs/>
+		public override string Name {
+			get {
+				if(String.IsNullOrEmpty(OverridedName))
+					return base.Name;
+				return OverridedName;
+			}
+		}
+
+		protected override Row MergeRows(Row leftRow, Row rightRow) {
+			var mergedRow = rowMerger(leftRow, rightRow);
+			if (ensureMergerReturnsNewRow) {
+				ensureMergedRowDifferentOfInput(leftRow, mergedRow, "left row == mergedRow, do not return left row from passed row merger");
+				ensureMergedRowDifferentOfInput(rightRow, mergedRow, "right row == mergedRow, do not return right row from passed row merger");
+			}
+			return mergedRow;
+		}
+
+		private void ensureMergedRowDifferentOfInput(Row inputRow, Row mergedRow, string message) {
+			if (inputRow == mergedRow) {
+				throw new InvalidOperationException(message);
+			}
+		}
+
+		protected override void SetupJoinConditions() {
+			JoinBuilder joinbuilder;
+			switch (joinType) {
+				case JoinType.Inner:
+					joinbuilder = InnerJoin;
+					break;
+				case JoinType.Left:
+					joinbuilder = LeftJoin;
+					break;
+				case JoinType.Right:
+					joinbuilder = RightJoin;
+					break;
+				case JoinType.Full:
+					joinbuilder = FullOuterJoin;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+
+			joinbuilder
+				.Left(leftColumnNames)
+				.Right(rightColumnNames);
+		}
+
+		protected override void LeftOrphanRow(Row row) {
+			if (LeftOrphanHandler != null)
+				LeftOrphanHandler(row);
+			else
+				base.LeftOrphanRow(row);
+		}
+
+		protected override void RightOrphanRow(Row row) {
+			if (RightOrphanHandler != null)
+				RightOrphanHandler(row);
+			else
+				base.RightOrphanRow(row);
+		}
+	}
+}
---
+++ Rhino.Etl.Tests/Joins/InlineJoinOperationEnsureMergerReturnsNewRowTestFixture.cs
@@ -0,0 +1,110 @@
+﻿namespace Rhino.Etl.Tests.Joins
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Rhino.Etl.Core;
+	using Rhino.Etl.Core.Operations;
+	using Rhino.Etl.Core.InlineOperations;
+	using Xunit;
+
+	public class InlineJoinOperationEnsureMergerReturnsNewRowTestFixture {
+
+		private static Func<Row, Row, Row> returnsLeftMerger = (left, right) => left;
+		private static Func<Row, Row, Row> returnsRightMerger = (left, right) => right;
+
+		FunctorWrapperOperation leftoperation;
+		FunctorWrapperOperation rightoperation;
+		IEnumerable<Row> leftrows;
+		IEnumerable<Row> rightrows;
+
+		public InlineJoinOperationEnsureMergerReturnsNewRowTestFixture() {
+			var leftdata = new[] { new { a = 1, b = "a" }, new { a = 2, b = "a" }, new { a = 3, b = "a" } };
+			var rightdata = new[] { new { a = 1, c = "a" }, new { a = 2, c = "a" }, new { a = 3, c = "a" } };
+
+			Func<object, IEnumerable<Row>> getRowsFromObjects = r => leftdata.Select(i => Row.FromObject(i));
+
+			leftrows = getRowsFromObjects(leftdata);
+			rightrows = getRowsFromObjects(rightdata);
+
+			leftoperation = new FunctorWrapperOperation(_ => leftrows);
+			rightoperation = new FunctorWrapperOperation(_ => rightrows);
+		}
+
+		[Fact]
+		public void WhenTrueAndMergerReturnsEitherLeftOrRightRowAnInvalidOperationIsThrown() {
+			string expectedMessagePortion;
+			Exception expectedException;
+			var columnsToJoinOn = new[] { "a" };
+
+			expectedMessagePortion = "left row == merged";
+			expectedException = getCatchExceptionBecauseOfBadRowMerger(columnsToJoinOn, returnsLeftMerger);
+			Assert.True(expectedException.Message.Contains(expectedMessagePortion));
+
+			expectedMessagePortion = "right row == merged";
+			expectedException = getCatchExceptionBecauseOfBadRowMerger(columnsToJoinOn, returnsRightMerger);
+			Assert.True(expectedException.Message.Contains(expectedMessagePortion));
+		}
+
+		[Fact]
+		public void WhenFalseAndMergerReturnsEitherLeftOrRightRowAnInvalidOperationIsNotThrown() {
+			var columnsToJoinOn = new[] { "a" };
+
+			var results = getResults(columnsToJoinOn, returnsLeftMerger);
+			foreach (var row in results) {
+				Assert.True(leftrows.Contains(row));
+			}
+
+			results = getResults(columnsToJoinOn, returnsRightMerger);
+			foreach (var row in results) {
+				Assert.True(rightrows.Contains(row));
+			}
+		}
+
+		private IEnumerable<Row> getResults(string[] columnsToJoinOn, Func<Row, Row, Row> rowMerger) {
+			var operation = prepareInlineJoinOperation(columnsToJoinOn, rowMerger, false);
+			operation.PrepareForExecution(new Rhino.Etl.Core.Pipelines.SingleThreadedPipelineExecuter());
+			return operation.Execute(null);
+		}
+
+		private InvalidOperationException getCatchExceptionBecauseOfBadRowMerger(string[] columnsToJoinOn, Func<Row, Row, Row> rowMerger) {
+			var operation = prepareInlineJoinOperation(columnsToJoinOn, rowMerger, true);
+			operation.PrepareForExecution(new Rhino.Etl.Core.Pipelines.SingleThreadedPipelineExecuter());
+			return Assert.Throws<InvalidOperationException>(
+				() =>
+					{
+						foreach (var _ in operation.Execute(null))
+						{
+						}
+					});
+		}
+
+		private InlineJoinOperation prepareInlineJoinOperation(string[] columnsToJoinOn, Func<Row, Row, Row> rowMerger, bool ensureMergerReturnsNewRow) {
+			return new InlineJoinOperation(
+				columnsToJoinOn,
+				columnsToJoinOn,
+				JoinType.Inner,
+				rowMerger,
+				leftoperation,
+				rightoperation,
+				ensureMergerReturnsNewRow
+				);
+		}
+
+		/// <summary>
+		/// inject any row provider, useful for unit test as now
+		/// </summary>
+		public class FunctorWrapperOperation : AbstractOperation {
+			private Func<IEnumerable<Row>, IEnumerable<Row>> executor;
+
+			public FunctorWrapperOperation(Func<IEnumerable<Row>, IEnumerable<Row>> executor) {
+				this.executor = executor;
+			}
+
+			public override IEnumerable<Row> Execute(IEnumerable<Row> rows) {
+				return executor(rows);
+			}
+		}
+
+	}
+}
